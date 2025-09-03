107d34abd440fda2315945eb671c94eb08e7dd2e
Reshma123HAMILPUR <reshmahamilpur27@gmail.com>
2025-09-03 14:17:00 +0530

    Test commit

---
+++ src/main/java/multilevel_inheritance/College.java
@@ -0,0 +1,11 @@
+package multilevel_inheritance;
+
+public class College extends University {
+    void providePlacements(){
+        System.out.println("reshma will learn inheritance");
+
+
+
+    }
+
+}
